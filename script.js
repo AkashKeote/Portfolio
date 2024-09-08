07bc79c4c0475c3b00829156a2a3b6d2ae9b08d7
Akash Keote <keoteakash@gmail.com>
2024-09-08 22:19:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 document.addEventListener('scroll', function() {
   const fadeElements = document.querySelectorAll('.fade-in');
-  const heroSection = document.querySelector('.hero-section'); // Select the hero-section
-
   fadeElements.forEach(el => {
-    // Check if the current element is not the hero-section
-    if (el !== heroSection) {
-      const position = el.getBoundingClientRect();
-      if (position.top < window.innerHeight) {
-        el.classList.add('scroll-in-view');
-      }
+    const position = el.getBoundingClientRect();
+    if (position.top < window.innerHeight) {
+      el.classList.add('scroll-in-view');
     }
   });
 });
@@ -16,19 +11,26 @@ document.addEventListener('scroll', function() {
 window.addEventListener('scroll', () => {
   let sections = document.querySelectorAll('section');
   let navLinks = document.querySelectorAll('nav .right a');
-  const heroSection = document.querySelector('.hero-section'); // Select the hero-section
 
   sections.forEach((section, index) => {
     let top = window.scrollY;
     let offset = section.offsetTop - 150;
     let height = section.offsetHeight;
 
-    // Only apply navigation link highlighting when not on the hero-section
-    if (section !== heroSection && top >= offset && top < offset + height) {
+    if (top >= offset && top < offset + height) {
       navLinks.forEach(link => {
         link.classList.remove('active');
       });
       navLinks[index].classList.add('active');
     }
+  });
+});
+
+document.addEventListener('scroll', function() {
+  const parallaxElements = document.querySelectorAll('.parallax');
+  
+  parallaxElements.forEach(el => {
+    const scrolled = window.scrollY;
+    el.style.backgroundPositionY = -(scrolled * 0.5) + 'px';
   });
 });
