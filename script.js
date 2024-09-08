94910864ecd6bb047f03abde9af21e8d950f283c
Akash Keote <keoteakash@gmail.com>
2024-09-08 22:16:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+document.addEventListener('scroll', function() {
+  const fadeElements = document.querySelectorAll('.fade-in');
+  const heroSection = document.querySelector('.hero-section'); // Select the hero-section
 
+  fadeElements.forEach(el => {
+    // Check if the current element is not the hero-section
+    if (el !== heroSection) {
+      const position = el.getBoundingClientRect();
+      if (position.top < window.innerHeight) {
+        el.classList.add('scroll-in-view');
+      }
+    }
+  });
+});
+
+window.addEventListener('scroll', () => {
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('nav .right a');
+  const heroSection = document.querySelector('.hero-section'); // Select the hero-section
+
+  sections.forEach((section, index) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+
+    // Only apply navigation link highlighting when not on the hero-section
+    if (section !== heroSection && top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+      navLinks[index].classList.add('active');
+    }
+  });
+});
